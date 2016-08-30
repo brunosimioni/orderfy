455fbdd6c4467346a3f20a9419b996f2d50f1db4
Bruno Simioni <brunosimioni@gmail.com>
2016-08-29 22:46:14 -0300

    wercker

--- src/main/java/com/brunosimioni/gateway/CatalogController.java
+++ src/main/java/com/brunosimioni/gateway/CatalogController.java
@@ -1,14 +1,27 @@
 package com.brunosimioni.gateway;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/catalog")
+public class CatalogController {
 
-public class Catalog {
-
-    
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	public List<ProductDTO> getAllProducts() {
+		
+		List<ProductDTO> products = new ArrayList<ProductDTO>();
+		products.add(new ProductDTO(1, "Product 1", "Category A"));
+		products.add(new ProductDTO(2, "Product 2", "Category A"));
+		products.add(new ProductDTO(3, "Product 3", "Category A"));
+		products.add(new ProductDTO(4, "Product 4", "Category B"));
+		products.add(new ProductDTO(5, "Product 5", "Category B"));
+		products.add(new ProductDTO(6, "Product 6", "Category B"));
+		
+		return products;
+	}
 }
--- src/main/java/com/brunosimioni/gateway/ProductDTO.java
+++ src/main/java/com/brunosimioni/gateway/ProductDTO.java
@@ -1,8 +1,16 @@
 package com.brunosimioni.gateway;
 
+import lombok.Data;
+
 @Data
 public class ProductDTO {
-    private Integer id;
-    private String  name;
-    private String  category;
+    public Integer id;
+    public String name;
+    public String category;
+    
+    public ProductDTO(Integer id, String name, String category) {
+    	this.id = id;
+    	this.name = name;
+    	this.category = category;
+    }
 }
