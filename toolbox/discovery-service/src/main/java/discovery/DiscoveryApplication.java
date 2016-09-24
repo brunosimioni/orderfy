abe789c6630f58d53c7bbc09f6eb4f31f28082db
Bruno Simioni <brunosimioni@gmail.com>
2016-09-23 22:31:14 -0300

    Docker compose up & running

--- toolbox/discovery-service/src/main/java/discovery/DiscoveryApplication.java
+++ toolbox/discovery-service/src/main/java/discovery/DiscoveryApplication.java
@@ -1,16 +1,14 @@
 package discovery;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-import com.netflix.discovery.shared.Application;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
-		}
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
 }
