f0777caba9965e0e1ab660c41dd1c6c1e8a5883a
Bruno Simioni <brunosimioni@gmail.com>
2016-09-23 21:43:33 -0300

    .

--- backend-services/catalog-service/src/main/java/catalog/CatalogApplication.java
+++ backend-services/catalog-service/src/main/java/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CatalogApplication {
 
 	public static void main(String[] args) {
--- backend-services/checkout-service/src/main/java/checkout/CheckoutApplication.java
+++ backend-services/checkout-service/src/main/java/checkout/CheckoutApplication.java
@@ -2,8 +2,10 @@ package checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CheckoutApplication {
 
 	public static void main(String[] args) {
