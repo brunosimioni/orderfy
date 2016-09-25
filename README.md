468ff1f6cc6cc4ff76256c10e153e94996b2ff3e
Bruno Simioni <brunosimioni@gmail.com>
2016-09-25 16:51:48 -0300

    docker-compose with wait-for-it dependencies

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ A simple proof-of-technology for an ecommerce platform
 - Travis CI continuous integration
 - Coveralls for static analysis and code lint
 - Spring Cloud Netflix OSS
-- Docker Compose for local deployment
+- Docker Compose for local deployment with wait-for-it dependencies
 - Kubernetes for cloud-native deployment
--- backend-services/customers-service/src/main/java/customers/CustomersApplication.java
+++ backend-services/customers-service/src/main/java/customers/CustomersApplication.java
@@ -2,8 +2,10 @@ package customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomersApplication {
 
 	public static void main(String[] args) {
