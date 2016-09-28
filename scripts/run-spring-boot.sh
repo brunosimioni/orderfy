03ba6bc2a80713de5efb6e247ed199d488970eb9
Bruno Simioni <brunosimioni@gmail.com>
2016-09-28 16:47:04 -0300

    docker with jmx

---
+++ scripts/run-spring-boot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+export MEM_ARGS="-Xmx64m -Xss256k"
+export JMX_ARGS="-Dcom.sun.management.jmxremote \
+         -Dcom.sun.management.jmxremote.port=3333 \
+         -Dcom.sun.management.jmxremote.ssl=false \
+         -Dcom.sun.management.jmxremote.local.only=false \
+         -Dcom.sun.management.jmxremote.authenticate=false"
+         
+java -jar $MEM_ARGS $JMX_ARGS $1
