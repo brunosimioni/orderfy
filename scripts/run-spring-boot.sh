c8d260ea4ae099490ad87f2966d86e6e7744e9ad
Bruno Simioni <brunosimioni@gmail.com>
2016-09-29 10:43:56 -0300

    jmx

--- scripts/run-spring-boot.sh
+++ scripts/run-spring-boot.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -x
+
 export MEM_ARGS="-Xmx64m -Xss256k"
-export JMX_ARGS=" -Dcom.sun.management.jmxremote.rmi.port=3333 \
+export JMX_ARGS="-Dcom.sun.management.jmxremote.rmi.port=3333 \
     -Dcom.sun.management.jmxremote=true \
     -Dcom.sun.management.jmxremote.port=3333 \ 
-    -Dcom.sun.management.jmxremote.ssl=false
-    -Dcom.sun.management.jmxremote.authenticate=false
-    -Dcom.sun.management.jmxremote.local.only=false
-    -Djava.rmi.server.hostname=localhost
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.sun.management.jmxremote.local.only=false \
+    -Djava.rmi.server.hostname=localhost"
          
 java -jar $MEM_ARGS $JMX_ARGS $1
