defe8a7ee25556bbe87735ca85bb850735ccc8a3
Bruno Simioni <brunosimioni@gmail.com>
2016-09-29 10:22:07 -0300

    docker jmx

--- scripts/run-spring-boot.sh
+++ scripts/run-spring-boot.sh
@@ -3,6 +3,7 @@ set -x
 export MEM_ARGS="-Xmx64m -Xss256k"
 export JMX_ARGS="-Dcom.sun.management.jmxremote \
          -Dcom.sun.management.jmxremote.port=3333 \
+         -Dcom.sun.management.jmxremote.rmi.port=3334 \
          -Dcom.sun.management.jmxremote.ssl=false \
          -Dcom.sun.management.jmxremote.local.only=false \
          -Dcom.sun.management.jmxremote.authenticate=false"
