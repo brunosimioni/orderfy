09d21ae012d0c9b882b9ab823bca65f73070580e
Bruno Simioni <brunosimioni@gmail.com>
2016-09-29 09:24:15 -0300

    traffic-generator with gatling.io

---
+++ traffic-generator/src/test/scala/traffic_generator/Recorder.scala
@@ -0,0 +1,14 @@
+package traffic_generator;
+
+import io.gatling.recorder.GatlingRecorder
+import io.gatling.recorder.config.RecorderPropertiesBuilder
+
+object Recorder extends App {
+
+	val props = new RecorderPropertiesBuilder
+	props.simulationOutputFolder(IDEPathHelper.recorderOutputDirectory.toString)
+	props.simulationPackage("traffic_generator")
+	props.bodiesFolder(IDEPathHelper.bodiesDirectory.toString)
+
+	GatlingRecorder.fromMap(props.build, Some(IDEPathHelper.recorderConfigFile))
+}
---
+++ traffic-generator/src/test/scala/traffic_generator/test/BasicSimulation.scala
@@ -0,0 +1,20 @@
+package traffic_generator.test;
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://nssp:8000/catalog-service/catalog")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+    .get("/"))
+    .pause(5)
+
+  setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
+}
