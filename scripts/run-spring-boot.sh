64e66a86b3da7df301e4e6f1d4248f400587c085
Bruno Simioni <brunosimioni@gmail.com>
2016-09-30 21:22:57 -0300

    JMX via Docker-compose Gatling with 3 scenarios Dynamic memory allocation per module

--- scripts/run-spring-boot.sh
+++ scripts/run-spring-boot.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-set -x
-export MEM_ARGS="-Xmx128m -Xss256k"
+
+export MEM_SIZE=$1
+export JAR_MODULE=$2
+export IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+export MEM_ARGS="-Xmx$1m -Xss256k"
 export JMX_ARGS="-Dcom.sun.management.jmxremote.rmi.port=3333 \
     -Dcom.sun.management.jmxremote=true \
     -Dcom.sun.management.jmxremote.port=3333 \
     -Dcom.sun.management.jmxremote.ssl=false \
     -Dcom.sun.management.jmxremote.authenticate=false \
     -Dcom.sun.management.jmxremote.local.only=false \
-    -Djava.rmi.server.hostname=localhost"
+    -Djava.rmi.server.hostname=$IP"
 
-java -jar $MEM_ARGS $JMX_ARGS $1
+echo "Executing java -jar $MEM_ARGS $JMX_ARGS $MEM_SIZE $JAR_MODULE"
+java -jar $MEM_ARGS $JMX_ARGS $JAR_MODULE
--- traffic-generator/src/test/scala/traffic_generator/test/BasicSimulation.scala
+++ traffic-generator/src/test/scala/traffic_generator/test/BasicSimulation.scala
@@ -1,20 +1,50 @@
 package traffic_generator.test;
 
+import scala.concurrent.duration._
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
-import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
+  
+  object Catalog {
+
+    val catalog = exec(http("Catalog")
+      .get("/catalog-service/catalog"))
+  }
+
+  object Customers {
+
+    val customers = exec(http("Customers")
+      .get("/customers-service/customers"))
+
+  }
+
+  object Checkout {
+
+    val checkout = exec(http("PlaceOrder")
+        .post("/checkout-service/checkout/placeorder"))
+  }
+
   val httpConf = http
-    .baseURL("http://nssp:8000/catalog-service/catalog")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+    .baseURL("http://localhost:8000")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("BasicSimulation")
-    .exec(http("request_1")
-    .get("/"))
-    .pause(5)
+  val scn = scenario("Orderfy").exec(Catalog.catalog, Customers.customers, Checkout.checkout)
 
-  setUp(scn.inject(atOnceUsers(60)).protocols(httpConf))
+  setUp(
+    scn.inject(
+      atOnceUsers(1),
+      constantUsersPerSec(80) during(10 seconds) randomized,
+      nothingFor(4 seconds),
+      rampUsersPerSec(50) to(100) during(5) randomized
+    ).protocols(httpConf)
+  )
 }
