09f107719f0f884eac4b25bb3349743c7afc784c
Bruno Simioni <brunosimioni@gmail.com>
2016-10-05 15:56:25 -0300

    turbine server done

--- backend-modules/checkout-service/src/main/java/checkout/CheckoutApplication.java
+++ backend-modules/checkout-service/src/main/java/checkout/CheckoutApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,7 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableSwagger2
-@EnableHystrixDashboard
 public class CheckoutApplication {
 
 	public static void main(String[] args) {
--- toolbox-modules/circuit-monitor-service/src/main/java/circuitmonitor/CircuitMonitorApplication.java
+++ toolbox-modules/circuit-monitor-service/src/main/java/circuitmonitor/CircuitMonitorApplication.java
@@ -2,10 +2,12 @@ package circuitmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableTurbineStream
+@EnableHystrixDashboard
 public class CircuitMonitorApplication {
 
 	public static void main(String[] args) {
