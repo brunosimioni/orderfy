d522ad7b8a828c977f7c58d7b95c798734a7373f
Bruno Simioni <brunosimioni@gmail.com>
2016-10-18 09:36:56 -0200

    Golang all set

--- backend-modules/lists-service/src/main.go
+++ backend-modules/lists-service/src/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func Hello(text string) string {
-	return text
-}
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Printf(Hello("Hello, world.\n"))
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
